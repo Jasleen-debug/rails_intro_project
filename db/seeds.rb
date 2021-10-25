a3b532c58415d731daa9e6f3a8952d7e72f3eeb4
Jasleen <jasleen0859@gmail.com>
2021-10-25 10:14:43 -0500

    Created CSV file and populated the database with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+require "date"
+Book.delete_all
+Publisher.delete_all
+
+file_name = Rails.root.join("db/top_books.csv")
+puts "Loading books from CSV file: #{file_name}"
+
+file_data = File.read(file_name)
+books = CSV.parse(file_data, headers: true, encoding: "utf-8")
+books.each do |book|
+  publisher = Publisher.find_or_create_by(name: book["publisher"])
+  if publisher&.valid?
+    book = publisher.books.create(
+      title:            book["title"],
+      isbn:             book["isbn"],
+      publication_date: book["publication_date"],
+      num_pages:        book["num_pages"],
+      average_listing:  book["average_listing"]
+    )
+    puts "Invalid book #{book['title']}" unless book&.valid?
+  else
+    puts "Invalid Publisher: #{book['publisher']} for book: #{book['title']}"
+  end
+end
+puts "Created #{Publisher.count} Publishers."
+puts "Created #{Book.count} books."
