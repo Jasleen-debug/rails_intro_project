dbeeac2009a40a732cb5206afb6b90b7e0e79923
Jasleen <jasleen0859@gmail.com>
2021-10-26 10:19:04 -0500

    Created controllers, views and edited routes for Books and Publishers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :books, only: %i[index show]
+  resources :publishers, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
